e78b9cb7fe1d8bb74ff1b2965f134673fedb1e0f
DESKTOP-OAKQKRM\Nicku <nycu_nycu20@yahoo.com>
2017-02-23 21:23:11 +0200

    First commit

---
+++ build/default/production/LCD_Test.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/LCD_Test.d  \
+ build/default/production/LCD_Test.p1:  \
+ LCD_Test.c  \
+lcd_hd44780_pic16.h 
---
+++ build/default/production/main.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/main.d  \
+ build/default/production/main.p1:  \
+ main.c  \
+lcd_hd44780_pic16.h 
